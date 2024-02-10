99002d16ae1995044e11fefc09f05d73b76c525f
Faisal2506 <shahfaiz3105@gmail.com>
2024-02-10 14:52:50 +0530

    Coaching screen 80% done

--- src/components/AvailableAcademies.js
+++ src/components/AvailableAcademies.js
@@ -1,14 +1,55 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Image, StyleSheet, Text, View } from 'react-native'
+import React from 'react';
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from 'react-native-responsive-screen';
+import {MaterialCommunityIcons} from 'react-native-vector-icons'
 
-const AvailableAcademies = () => {
+
+const AvailableAcademies = ({title,address,picture,sports}) => {
   return (
-    <View>
-      <Text>AvailableAcademies</Text>
+    <View style={styles.mainContainer}>
+      <Text style={styles.title}>{title}</Text>
+      <Text style={styles.address}>{address}</Text>
+      <Image 
+        source={picture} style={styles.picture}
+      />
+      <View style={styles.sportsContainer}>
+        <MaterialCommunityIcons 
+          name="baseball"
+          size={10}
+          color="#000"
+        />
+        <Text style={styles.sports}>{sports}</Text>
+      </View>
     </View>
   )
 }
 
 export default AvailableAcademies
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  mainContainer:{
+    flex:1,
+    padding:wp(2),
+    // borderWidth:1
+  },
+  title: {
+    // marginBottom:hp(0)
+    fontSize:hp(2)
+  },
+  picture:{
+    height:hp(19),
+    borderRadius:wp(4),
+    marginTop:hp(1)
+  },
+  sportsContainer:{
+    flexDirection:'row',
+    marginTop:hp(1),
+    alignItems:'center'
+  },
+  sports:{
+    marginLeft:wp(3)
+  }
+})
---
+++ src/constants/kits.js
@@ -0,0 +1,32 @@
+export const kits = [
+  {
+    key: '1',
+    picture:require('../../assets/images/cricketKit.png'),
+    title:"Cricket"
+  },
+  {
+    key: '2',
+    picture:require('../../assets/images/footballKit.jpg'),
+    title:"Football"
+  },
+  {
+    key: '3',
+    picture:require('../../assets/images/badmintonKit.jpg'),
+    title:"Badminton"
+  },
+  {
+    key: '4',
+    picture:require('../../assets/images/swimmingKit.jpg'),
+    title:"Tennis"
+  },
+  {
+    key: '5',
+    picture:require('../../assets/images/tennisKit.jpg'),
+    title:"Tennis"
+  },
+  {
+    key: '6',
+    picture:require('../../assets/images/cricketKit.png'),
+    title:"Table Tennis"
+  }
+]
